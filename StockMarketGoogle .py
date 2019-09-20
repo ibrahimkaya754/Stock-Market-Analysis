d6fb2825cbe8e8578917c4fdb32f9dcc689fd325
ikaya <ikaya754@gmail.com>
2019-09-20 08:57:54 +0300

    changes on pandas dataframe

--- StockMarketGoogle .py
+++ StockMarketGoogle .py
@@ -7,12 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from sklearn.preprocessing import MinMaxScaler
 
 
 #%%
 dataset = pd.read_csv('Google_Stock_Price_Train.csv',index_col="Date",parse_dates=True)
-
-
 #%%
 dataset.head()
 #%%
@@ -26,49 +29,34 @@ dataset.info()
 #%%
 dataset['Open'].plot(figsize=(16,6))
 #%%
+# The error here is about to have ',' inside some of the numeric values
 dataset['Close'] = pd.to_numeric(dataset.Close)
-
 #%%
 # convert column "a" of a DataFrame
 dataset["Close"] = dataset["Close"].str.replace(',', '').astype(float)
-
-
 #%%
 dataset["Volume"] = dataset["Volume"].str.replace(',', '').astype(float)
-
 
 #%%
 # 7 day rolling mean
 dataset.rolling(7).mean().head(20)
-
-
 #%%
 dataset['Open'].plot(figsize=(16,6))
 dataset.rolling(window=30).mean()['Close'].plot()
-
-
 #%%
 dataset['Close: 30 Day Mean'] = dataset['Close'].rolling(window=30).mean()
 dataset[['Close','Close: 30 Day Mean']].plot(figsize=(16,6))
-
-
 #%%
 # Optional specify a minimum number of periods
 dataset['Close'].expanding(min_periods=1).mean().plot(figsize=(16,6))
 
-
 #%%
 training_set=dataset['Open']
 training_set=pd.DataFrame(training_set)
-
-
 #%%
 # Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+sc                  = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
-
-
 #%%
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
@@ -76,27 +64,16 @@ y_train = []
 for i in range(60, 1258):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
-X_train, y_train = np.array(X_train), np.array(y_train)
-
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-
-
 #%%
 # Part 2 - Building the RNN
-
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-
 
 #%%
 # Initialising the RNN
 regressor = Sequential()
-
-
 #%%
 # Adding the first LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
