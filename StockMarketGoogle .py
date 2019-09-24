d4a5ef4cfabba57d850c8c1c1fd3a15d3cb8b7ac
ikaya <ikaya754@gmail.com>
2019-09-24 08:51:37 +0300

    ---

--- StockMarketGoogle .py
+++ StockMarketGoogle .py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Input
 from keras.layers import LSTM
 from keras.layers import Dropout
 from sklearn.preprocessing import MinMaxScaler
-
+from params import params
 
 #%%
 dataset = pd.read_csv('Google_Stock_Price_Train.csv',index_col="Date",parse_dates=True)
@@ -30,7 +30,7 @@ dataset.info()
 dataset['Open'].plot(figsize=(16,6))
 #%%
 # The error here is about to have ',' inside some of the numeric values
-dataset['Close'] = pd.to_numeric(dataset.Close)
+#dataset['Close'] = pd.to_numeric(dataset.Close)
 #%%
 # convert column "a" of a DataFrame
 dataset["Close"] = dataset["Close"].str.replace(',', '').astype(float)
@@ -68,110 +68,104 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-#%%
-# Part 2 - Building the RNN
-
-#%%
-# Initialising the RNN
-regressor = Sequential()
-#%%
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
-
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
-
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
-
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
-
-# Adding the output layer
-regressor.add(Dense(units = 1))
-
-
-#%%
-# Compiling the RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
-# Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-
-
-#%%
-# Part 3 - Making the predictions and visualising the results
-
+print(X_train.shape)
+#%%   VALIDATION / TEST SET
 # Getting the real stock price of 2017
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv',index_col="Date",parse_dates=True)
-
-
-#%%
 real_stock_price = dataset_test.iloc[:, 1:2].values
-
-
-#%%
 dataset_test.head()
-
-
-#%%
 dataset_test.info()
-#%%
 dataset.columns
-#%%
 dataset_test["Volume"] = dataset_test["Volume"].str.replace(',', '').astype(float)
-
-
-#%%
 test_set=dataset_test['Open']
 test_set=pd.DataFrame(test_set)
-
-
-#%%
 test_set.info()
-
-
+#%% Part 2 - Building the RNN
+parameters     = {}
+list_unit_size = [20,50,80,110]
+list_dropout   = [0.1,0.15,0.20,0.25,0.30]
+list_layer_no  = [1,2,3,4,5]
+list_epoch     = [250,500,1000]
+list_batch     = [16,32,64,128]
 #%%
-# Getting the predicted stock price of 2017
-dataset_total = pd.concat((dataset['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = inputs.reshape(-1,1)
-inputs = sc.transform(inputs)
-X_test = []
-for i in range(60, 80):
-    X_test.append(inputs[i-60:i, 0])
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-predicted_stock_price = regressor.predict(X_test)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+# Initialising the RNN
+def model(parameters):
+    Layer         = []
+    
+    Input_LSTM = Input(shape(X_train.shape[1],1))
+    Layer.append(LSTM(parameters['unit_size'])(Input_LSTM)
+    for layer_no in range(parameters['layer_no']):
+        Layer
 
 
+
+
+
+    regressor = Sequential()
+    # Adding the first LSTM layer and some Dropout regularisation
+    regressor.add(LSTM(units = parameters['unit_size'], return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    regressor.add(Dropout(parameters['dropout']))
+
+    for layer_no in range(int(parameters['layer_no'])):
+        regressor.add(LSTM(units= parameters['unit_size'],return_sequences=True))
+        regressor.add(Dropout(parameters['dropout']))
+
+    # Adding the output layer
+    regressor.add(Dense(units = 1))
+
+    # Compiling the RNN
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    
+    return regressor
 #%%
-predicted_stock_price=pd.DataFrame(predicted_stock_price)
-predicted_stock_price.info()
+exp_no = 1
+for layer_no in list_layer_no:
+    parameters['layer_no'] = layer_no
+    for dropout in list_dropout:
+        parameters['dropout'] = dropout
+        for unit_size in list_unit_size:
+            parameters['unit_size'] = unit_size
+            for epoch in list_epoch:
+                parameters['epoch'] = epoch
+                for batch in list_batch:
+                    parameters['batch'] = batch
+                    model_LSTM = model(parameters)   
+                    print(X_train.shape)
+                    # Fitting the RNN to the Training set
+                    model_LSTM.fit(X_train, y_train, epochs = parameters['epoch'], batch_size = parameters['batch'])         
+                    
+                    # Part 3 - Making the predictions and visualising the results
 
+                    #%%
+                    # Getting the predicted stock price of 2017
+                    dataset_total = pd.concat((dataset['Open'], dataset_test['Open']), axis = 0)
+                    inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+                    inputs = inputs.reshape(-1,1)
+                    inputs = sc.transform(inputs)
+                    X_test = []
+                    for i in range(60, 80):
+                        X_test.append(inputs[i-60:i, 0])
+                    X_test = np.array(X_test)
+                    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+                    predicted_stock_price = model_LSTM.predict(X_test)
+                    predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+                    #%%
+                    predicted_stock_price=pd.DataFrame(predicted_stock_price)
+                    predicted_stock_price.info()
+                    #%%
+                    # Visualising the results
+                    plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+                    plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+                    plt.title('Google Stock Price Prediction')
+                    plt.xlabel('Time')
+                    plt.ylabel('Google Stock Price')
+                    plt.legend()
+                    plt.show()
 
-#%%
+                    #%%
+                    params(experiment_name='exp_lstm'+str(exp_no),parameters=parameters,filename = 'params_stock_market_lstm.txt')
 
-# Visualising the results
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
-plt.title('Google Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
-plt.legend()
-plt.show()
-
-
-#%%
-
-
-
-#%%
+                    #%%
 
 
 
